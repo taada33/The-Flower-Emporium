cae3e81ce8e9db2d23a2f750a7bc9b53ca92aeaa
MohamedAbdullahi2 <118404373+MohamedAbdullahi2@users.noreply.github.com>
2023-03-19 18:37:28 -0400

    fixed bug

--- server.js
+++ server.js
@@ -1,96 +1,46 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
 const path = require('path');
+const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-// Set up session middleware
-app.use(session({
-  secret: 'mySecretKey',
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 60 * 60 * 1000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
   resave: false,
-  saveUninitialized: true
-}));
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Set up handlebars as the view engine
-// app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
-// app.set('view engine', 'handlebars');
+// turn on routes
+app.use(routes);
 
-// app.get('/', (req, res) => {
-//     res.render('main', {
-//     });
-// });
-app.get('/login', (req, res, next) => {
-  try {
-    res.render('login', { title: 'Login' });
-  } catch (error) {
-    next(error);
-  }
-});
-
-app.get('/shopping-cart', (req, res, next) => {
-  try {
-    const cart = req.session.cart || [];
-    const totalPrice = cart.reduce((acc, item) => acc + (item.price * item.quantity), 0);
-    res.render('shopping-cart', { title: 'Shopping Cart', cart: cart || [], totalPrice });
-  } catch (error) {
-    next(error);
-  }
-});
-
-app.get('/products', (req, res, next) => {
-  try {
-    const products = [
-      {
-        name: 'Product 1',
-        description: 'This is the first product',
-        price: 9.99,
-        image: 'https://example.com/product1.jpg'
-      },
-      {
-        name: 'Product 2',
-        description: 'This is the second product',
-        price: 19.99,
-        image: 'https://example.com/product2.jpg'
-      },
-      {
-        name: 'Product 3',
-        description: 'This is the third product',
-        price: 29.99,
-        image: 'https://example.com/product3.jpg'
-      }
-    ];
-
-    res.render('products', { title: 'Products', products: products || [] });
-  } catch (error) {
-    next(error);
-  }
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
-});
-
-
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
-});
-
-
-app.listen(3000, () => {
-  console.log('Server listening on port 3000!');
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
